016e16275d38bbb8c8f2f13c6754b075ac88a878
ramfery <47457744+ramfery@users.noreply.github.com>
2019-02-08 12:29:52 -0600

    Integración de proyecto

---
+++ js/main.js
@@ -0,0 +1,51 @@
+console.log("hola todos");
+console.log("espero estemos disfrutando y aprendiendo");
+console.log("en este Kodecamp!!!");
+
+var x = 5;
+var y = 10;
+var someName ="Israel Salinas"
+var weAreLearning = true;
+var someFloat = 3.23423523
+
+var someArray = [
+	"mazda",
+	"honda",
+	"toyota",
+	"renault"
+]
+
+var someObject = {
+	company:"mazda",
+	model:"mazda 6",
+	color:"red",
+	horsePower:"250hp",
+	cilinders:"6"
+}
+
+var mazdaCars = [
+	{
+		company:"mazda",
+		model:"mazda 6",
+		color:"red",
+		horsePower:"250hp",
+		cilinders:"6"
+	},{
+		company:"mazda",
+		model:"mazda 2",
+		color:"blue",
+		horsePower:"110hp",
+		cilinders:"4"
+	}
+]
+
+function printCars(carIndex){
+	for(i=0; imazdaCars.lenght; i++){
+		debugger;
+	var ibjeto1 = mazdaCars[carIndex];
+	console.log(objeto1);
+	var carModel = objeto1.model
+	sds
+
+}
+
